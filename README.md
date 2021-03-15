5e8b3a5a8391059f8fb7fbc899ab0f1b58902bac
valentine195 <38669521+valentine195@users.noreply.github.com>
2021-03-15 14:49:26 -0400

    0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# obsidian-dice-roller
- Inline dice rolling for Obsidian.md
+## Dice Roller
+Dice rolling for Obsidian.md
+
+### Usage
+Simply place {dice: XdX} and in preview mode it will be replaced with the result of the dice rolls. In preview mode, the result can be re-rolled by clicking on it.
+
+#### Dice Formula
+
+The parser supports addition, subtraction, multiplication, division, and exponents of an arbitrary number of dice or static numbers. Spaces are removed before the formula is parsed.
+
+There is full order-of-operations support, so it can even nested into parentheses!
+
+| Examples                            |
+| ----------------------------------- |
+| {dice: 1d2}                         |
+| {dice: 3d4 + 2}                     |
+| {dice: 1d12 + 1d10 + 2}             |
+| {dice: 3d4+3d4-(3d4 * 1d4) - 2^1d2} |
+
+#### Customization
+
+The result is given the `.dice-roller` class. Override it to customize the look of the result.
+
+### Coming Soon
+
+- Preview of actual dice results when hovered
+## Installation
+
+
+### From GitHub
+- Download the Latest Release from the Releases section of the GitHub Repository
+- Extract the plugin folder from the zip to your vault's plugins folder: `<vault>/.obsidian/plugins/`  
+Note: On some machines the `.obsidian` folder may be hidden. On MacOS you should be able to press `Command+Shift+Dot` to show the folder in Finder.
+- Reload Obsidian
+- If prompted about Safe Mode, you can disable safe mode and enable the plugin.
+Otherwise head to Settings, third-party plugins, make sure safe mode is off and
+enable the plugin from there.
+
+#### Updates
+You can follow the same procedure to update the plugin
+
+## Warning
+
+This plugin comes with no guarantee of stability and bugs may delete data.
+Please ensure you have automated backups.
+## TTRPG plugins
+
+If you're using Obsidian to run/plan a TTRPG, you may find my other plugin useful:
+
+- [Obsidian Leaflet](https://github.com/valentine195/obsidian-leaflet-plugin) - Add interactive maps to Obsidian.md notes
+   
+
--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,106 @@
 import {
-	Notice,
 	Plugin,
 	MarkdownPostProcessorContext,
-	ExtraButtonComponent,
 } from "obsidian";
+//@ts-ignore
+import lexer from 'lex';
 
 import { faDice } from "@fortawesome/free-solid-svg-icons";
 import { icon } from "@fortawesome/fontawesome-svg-core";
 
 import './main.css';
 
+
+class Parser {
+	table: any;
+	constructor(table: any) {
+		this.table = table;
+	}
+	parse(input: string[]) {
+		var length = input.length,
+			table = this.table,
+			output = [],
+			stack = [],
+			index = 0;
+
+		while (index < length) {
+			var token = input[index++];
+
+			switch (token) {
+				case "(":
+					stack.unshift(token);
+					break;
+				case ")":
+					while (stack.length) {
+						var token = stack.shift();
+						if (token === "(") break;
+						else output.push(token);
+					}
+
+					if (token !== "(")
+						throw new Error("Mismatched parentheses.");
+					break;
+				default:
+					if (table.hasOwnProperty(token)) {
+						while (stack.length) {
+							var punctuator = stack[0];
+
+							if (punctuator === "(") break;
+
+							var operator = table[token],
+								precedence = operator.precedence,
+								antecedence = table[punctuator].precedence;
+
+							if (precedence > antecedence ||
+								precedence === antecedence &&
+								operator.associativity === "right") break;
+							else output.push(stack.shift());
+						}
+
+						stack.unshift(token);
+					} else output.push(token);
+			}
+		}
+
+		while (stack.length) {
+			var token = stack.shift();
+			if (token !== "(") output.push(token);
+			else throw new Error("Mismatched parentheses.");
+		}
+
+		return output;
+	}
+}
+
 export default class MyPlugin extends Plugin {
+	lexer: lexer;
+	parser: Parser;
 	async onload() {
 		console.log("DiceRoller plugin loaded");
-
-		this.addRibbonIcon("dice", "DiceRoller", () => {
-			new Notice("This is a notice!");
-		});
-
-		this.addCommand({
-			id: "roll-dice",
-			name: "Roll Dice",
-			checkCallback: (checking: boolean) => {},
-		});
 
 		this.registerMarkdownPostProcessor(
 			(el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
 				if (el.innerText.includes("{dice")) {
 					//find text node
-					function findTextNode(el: Node, search?: string): Node[] {
-						let list: Node[] = [];
-						if (
-							el.nodeType == 3 &&
-							el.textContent != "\n" &&
-							el.textContent.includes(search)
-						) {
-							list.push(el);
-						}
 
-						if (el.hasChildNodes()) {
-							el.childNodes.forEach(node => {
-								list.push(...findTextNode(node, search));
-							});
-						}
-
-						return list;
-					}
-
-					let nodeList = findTextNode(el, "{dice:");
+					let nodeList = this.findTextNode(el, "{dice:");
 
 					nodeList.forEach(node => {
-						/* let text =  */
 						let parent = node.parentElement;
-						node.parentElement.innerHTML = node.parentElement.innerHTML.replace(
+						/* node.parentElement.innerHTML = node.parentElement.innerHTML.replace(
 							/\{dice:\s*(\d+d\d+)\s*([\+\-\*])*\s*(\d+)*\}/g,
 							(match, p1, p2, p3, offset, string): string => {
 								let [, amt, face] = p1.match(/(\d+)d(\d+)/);
 								let modifier = p2;
 								let end = p3;
-
+	
 								let results = getResults(
 									+amt,
 									+face,
 									modifier,
 									+end
 								);
-
+	
 								let container = createDiv().createDiv({
 									cls: "dice-roller",
 									title: ` (${p1}${p2 ? p2 : ""}${
@@ -77,12 +112,37 @@ export default class MyPlugin extends Plugin {
 									"data-face": +face,
 									"data-modifier": modifier,
 									"data-end": +end,
+									"data-source": match
 								});
 								let diceSpan = container.createSpan();
-
+	
 								diceSpan.innerText = `${results.reduce(
 									(a, c) => a + c
 								)}`;
+	
+								container
+									.createDiv({ cls: "dice-roller-button" })
+									.appendChild(
+										icon(faDice).node[0]
+									) as HTMLElement;
+	
+								return container.parentElement.innerHTML;
+							}
+						); */
+						node.parentElement.innerHTML = node.parentElement.innerHTML.replace(
+							/{dice:([\s\S]+?)}/g,
+							(match, dice): string => {
+								dice = dice.split(' ').join('').trim()
+								let results = this.parseDice(dice);
+
+								let container = createDiv().createDiv({
+									cls: "dice-roller",
+								});
+								container.setAttr('data-dice', dice);
+
+								let diceSpan = container.createSpan();
+
+								diceSpan.innerText = `${results}`;
 
 								container
 									.createDiv({ cls: "dice-roller-button" })
@@ -101,32 +161,13 @@ export default class MyPlugin extends Plugin {
 									"click",
 									() => {
 										let {
-											amt,
-											face,
-											modifier,
-											end,
+											dice
 										} = (diceElement as HTMLElement).dataset;
 
-										let results = getResults(
-											+amt,
-											+face,
-											modifier,
-											+end
-										);
-										diceElement.setAttribute(
-											"title",
-											`(${amt}d${face}${
-												modifier != "undefined"
-													? modifier
-													: ""
-											}${
-												!isNaN(+end) ? end : ""
-											}): ${results.join(", ")}`
-										);
+										let results = this.parseDice(dice.split(' ').join(''));
+										console.log(dice);
 										(diceElement
-											.children[0] as HTMLElement).innerText = `${results.reduce(
-											(a, c) => a + c
-										)}`;
+											.children[0] as HTMLElement).innerText = `${results}`;
 									}
 								);
 							});
@@ -134,39 +175,111 @@ export default class MyPlugin extends Plugin {
 				}
 			}
 		);
+
+		this.lexer = new lexer;
+
+		this.lexer.addRule(/\s+/, function () {
+			/* skip whitespace */
+		});
+
+		this.lexer.addRule(/([0-9]\d*)+[Dd]?([0-9]\d*)?/, function (lexeme: string) {
+			return lexeme; // symbols
+		});
+
+		this.lexer.addRule(/[\(\^\+\-\*\/\)]/, function (lexeme: string) {
+			return lexeme; // punctuation (i.e. "(", "+", "-", "*", "/", ")")
+		});
+
+		var exponent = {
+			precedence: 3,
+			associativity: "right"
+		};
+
+		var factor = {
+			precedence: 2,
+			associativity: "left"
+		};
+
+		var term = {
+			precedence: 1,
+			associativity: "left"
+		};
+
+		this.parser = new Parser({
+			"+": term,
+			"-": term,
+			"*": factor,
+			"/": factor,
+			"^": exponent
+		});
+
 	}
 
 	onunload() {
 		console.log("DiceRoller unloaded");
 	}
-}
 
-const getResults = function (
-	amt: number,
-	face: number,
-	modifier: string,
-	end: number
-): number[] {
-	return [...Array(+amt)].map((): number => {
-		let n = Math.floor(Math.random() * +face) + 1;
-
-		switch (modifier) {
-			case "+": {
-				n += +end;
-				break;
-			}
-			case "-": {
-				n -= +end;
-				break;
-			}
-			case "*": {
-				n *= +end;
-				break;
-			}
-			default: {
-			}
+	findTextNode(el: Node, search?: string): Node[] {
+		let list: Node[] = [];
+		if (
+			el.nodeType == 3 &&
+			el.textContent != "\n" &&
+			el.textContent.includes(search)
+		) {
+			list.push(el);
 		}
 
-		return 1 * n;
-	});
-};
+		if (el.hasChildNodes()) {
+			el.childNodes.forEach(node => {
+				list.push(...this.findTextNode(node, search));
+			});
+		}
+
+		return list;
+	}
+	operators: any = {
+		"+": (a: number, b: number): number => a + b,
+		"-": (a: number, b: number): number => a - b,
+		"*": (a: number, b: number): number => a * b,
+		"/": (a: number, b: number): number => a / b,
+		"^": (a: number, b: number): number => { return Math.pow(a, b) }
+	};
+	parseDice(text: string): number {
+
+		let stack: number[] = [];
+		this.parse(text).forEach(d => {
+			switch (d) {
+				case "+":
+				case "-":
+				case "*":
+				case "/":
+				case "^":
+					let b = stack.pop();
+					let a = stack.pop();
+					stack.push(this.operators[d](a, b));
+					break;
+				default:
+					stack.push(this.roll(d).reduce((acc, curr) => acc + curr, 0));
+			}
+		});
+		return stack[0];
+	}
+
+	roll(dice: string): number[] {
+		if (!/([0-9]\d*)[dD]?([0-9]\d*)?/.test(dice)) return;
+		let [, amount, faces] = dice.match(/([0-9]\d*)[dD]?([0-9]\d*)?/);
+		if (!faces) return [Number(amount)];
+		let result = [...Array(Number(amount))].map(() => Math.floor(Math.random() * Number(faces)) + 1);
+
+		return result;
+	}
+
+	parse(input: string): string[] {
+		this.lexer.setInput(input);
+		var tokens = [], token;
+		while (token = this.lexer.lex()) tokens.push(token);
+		return this.parser.parse(tokens);
+	}
+
+}
+
