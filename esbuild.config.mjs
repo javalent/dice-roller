7b3e424c0ecbb8b4a24ff8dc36bfb2347d5e6d5f
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-01-14 08:16:15 -0500

    update to target es2020

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -25,7 +25,7 @@ esbuild
         external: ["obsidian", "electron", ...builtins],
         format: "cjs",
         watch: !prod,
-        target: "es2016",
+        target: "es2020",
         logLevel: "info",
         sourcemap: prod ? false : "inline",
         treeShaking: true,
