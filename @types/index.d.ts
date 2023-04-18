1978a32f71d826eac7c061edbdfbeb0ac0179faf
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-04-18 17:48:53 -0400

    fix: Update types for npm package

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,16 +1,35 @@
-interface DiceRoller {
-    getRoller(roll: string, source?: string): Promise<BasicRoller>;
+export interface DiceRollerAPI {
+    registerSource(source: string, options: RollerOptions): void;
     getRollerSync(roll: string, source?: string): BasicRoller;
+    getRoller(roll: string, source?: string): Promise<BasicRoller>;
 }
 
+export enum ExpectedValue {
+    None = "None",
+    Average = "Average",
+    Roll = "Roll"
+}
+export interface RollerOptions {
+    showDice?: boolean;
+    shouldRender?: boolean;
+    showFormula?: boolean;
+    expectedValue?: ExpectedValue;
+    text?: string;
+    showParens?: boolean;
+    formulaAfter?: boolean;
+}
 interface BasicRoller {
     result: any;
     roll(): Promise<any>;
     rollSync(): number;
 }
 
-interface StackRoller extends BasicRoller {
+export interface StackRoller extends BasicRoller {
     result: number;
 }
-export default DiceRoller;
-export { StackRoller };
+
+export default interface DiceRoller {
+    getRoller(roll: string, source?: string): Promise<BasicRoller>;
+    getRollerSync(roll: string, source?: string): BasicRoller;
+    getAPI(): DiceRollerAPI;
+}
