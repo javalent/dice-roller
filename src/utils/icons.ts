8a0cc2be27aff9328548ce37a5f7acf47fc42304
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-04-22 09:40:37 -0400

    fix: Fixes some icon definitions

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -3,10 +3,10 @@ export enum Icons {
 
     COPY = "copy",
 
-    MINUS = "square-minus",
-    PLUS = "square-plus",
+    MINUS = "minus-square",
+    PLUS = "plus-square",
 
-    SAVE = "plus-with-circle",
+    SAVE = "save",
     EDIT = "edit",
     DONE = "checkmark",
     CANCEL = "cross",
