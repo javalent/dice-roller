b1438efe9b3292848e269405e3c76d41229905d3
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-10-23 12:57:25 -0400

    fix: Show Dice Roller version on load

--- src/main.ts
+++ src/main.ts
@@ -37,11 +37,11 @@ export default class DiceRollerPlugin extends Plugin {
         };
     }
     async onload() {
-        console.log("DiceRoller plugin loaded");
         await this.loadSettings();
+        console.log(`DiceRoller v${this.data.version} loaded`);
 
         DiceRenderer.setData(this.getRendererData());
-        
+
         this.api.initialize(this.data, this.app);
 
         window["DiceRoller"] = this.api;
